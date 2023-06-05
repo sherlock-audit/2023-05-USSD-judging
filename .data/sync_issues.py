024dd804116dfcd5aa9f81db24db832fa234d301
Rares Stanciu <1048185+rcstanciu@users.noreply.github.com>
2023-06-05 22:25:00 +0300

    Update sync_issues.py

--- .data/sync_issues.py
+++ .data/sync_issues.py
@@ -4,7 +4,7 @@ import re
 import time
 from functools import lru_cache, wraps
 
-from github import Github, Issue, Repository
+from github import ContentFile, Github, Issue, Repository
 from github.GithubException import (
     GithubException,
     RateLimitExceededException,
@@ -59,6 +59,26 @@ class RepositoryExtended(Repository.Repository):
             setattr(repo, func, github_retry_on_rate_limit(getattr(repo, func)))
         return repo
 
+
+class ContentFileExtended(ContentFile.ContentFile):
+    @classmethod
+    def cast(cls, content_file: ContentFile):
+        content_file.__class__ = ContentFileExtended
+
+        for func in ["_completeIfNotSet"]:
+            setattr(content_file, func, github_retry_on_rate_limit(getattr(content_file, func)))
+        return content_file
+
+class GithubExtended(Github):
+    @classmethod
+    def cast(cls, github: Github):
+        github.__class__ = GithubExtended
+
+        for func in ["get_repo"]:
+            setattr(github, func, github_retry_on_rate_limit(getattr(github, func)))
+        return github
+
+github = GithubExtended.cast(github)
 
 # Issues list. Each issue is in the format:
 # {
@@ -120,12 +140,13 @@ def process_directory(repo, path):
             files = [item]
 
         for file in files:
+            file = ContentFileExtended.cast(file)
             if "best" in file.name:
                 issue_id = int(file.name.replace("-best.md", ""))
                 parent = issue_id
             else:
                 issue_id = int(file.name.replace(".md", ""))
-
+            
             body = file.decoded_content.decode("utf-8")
             auditor = body.split("\n")[0]
             issue_title = re.match(r"^(?:[#\s]+)(.*)$", body.split("\n")[4]).group(1)
@@ -175,24 +196,7 @@ def main():
     run_number = int(os.environ.get("GITHUB_RUN_NUMBER"))
 
     repo = RepositoryExtended.cast(github.get_repo(repo))
-
-    process_directory(repo, "")
-    # Sort them by ID so we match the order
-    # in which GitHub Issues created
-    issues = dict(sorted(issues.items(), key=lambda item: item[1]["id"]))
-
-    # Ensure issue IDs are sequential
-    actual_issue_ids = list(issues.keys())
-    expected_issue_ids = list(range(1, max(actual_issue_ids) + 1))
-    missing_issue_ids = [x for x in expected_issue_ids if x not in actual_issue_ids]
-    assert (
-        actual_issue_ids == expected_issue_ids
-    ), "Expected issues %s actual issues %s. Missing %s" % (
-        expected_issue_ids,
-        actual_issue_ids,
-        missing_issue_ids,
-    )
-
+    
     labels = [
         {
             "name": "High",
@@ -278,7 +282,7 @@ def main():
     label_names = [x["name"] for x in labels]
 
     # Create the labels if it's the first time this action is run
-    if run_number == 1:
+    if run_number == 3:
         print("Creating issue labels")
         existing_labels = list(repo.get_labels())
         existing_label_names = [x.name for x in existing_labels]
@@ -291,6 +295,23 @@ def main():
                 repo.create_label(**label)
     else:
         print("Skipping creating labels.")
+
+    process_directory(repo, "")
+    # Sort them by ID so we match the order
+    # in which GitHub Issues created
+    issues = dict(sorted(issues.items(), key=lambda item: item[1]["id"]))
+
+    # Ensure issue IDs are sequential
+    actual_issue_ids = list(issues.keys())
+    expected_issue_ids = list(range(1, max(actual_issue_ids) + 1))
+    missing_issue_ids = [x for x in expected_issue_ids if x not in actual_issue_ids]
+    assert (
+        actual_issue_ids == expected_issue_ids
+    ), "Expected issues %s actual issues %s. Missing %s" % (
+        expected_issue_ids,
+        actual_issue_ids,
+        missing_issue_ids,
+    )
 
     # Sync issues
     for issue_id, issue in issues.items():
